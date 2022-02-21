bdf624dcc4e20e7c4ee4209f121a457e9bed5f59
Mattias Lij <mattias.lij@teliacompany.com>
2022-02-21 19:07:07 +0100

    Added UserReply

--- Scripts/SqlDb/GenerateTraningRows.ps1
+++ Scripts/SqlDb/GenerateTraningRows.ps1
@@ -1,7 +1,8 @@
+function SqlStatement {
+    param (
+        [datetime]$date
+    )
 
-for ($i = 0; $i -lt 25; $i++) {
-    $date = Get-date
-    $date = $date.AddDays(7 * $i)
     if ($date.Day -lt 10) {
         $day = "0$($date.Day)"
     }
@@ -14,4 +15,37 @@ for ($i = 0; $i -lt 25; $i++) {
     Write-Host "select NEWID(), @EventDate, 1, @GameAddressId" 
     Write-Host "where not exists (select 1 from [Events] where Date = @EventDate and EventType = 1 and AddressId = @GameAddressId)" 
     Write-Host
+}
+
+function PostToApi {
+    param (
+        [datetime]$date
+    )
+    
+    $body = [ordered]@{
+        date      = $date
+        eventType = 1
+        address   = @{
+            id = "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+        }
+        users     = [array][ordered]@{
+            userId    = "0cafdfe9-614f-495b-b672-61508baa3dbc"
+            userName  = "Matte"
+            userReply = 1
+            isOwner   = $true
+        }
+    }
+
+    $response = Invoke-RestMethod -Uri 'https://sportplannerapi.azurewebsites.net/api/event' -Method 'POST' -Body (ConvertTo-Json $body -Depth 32) -ContentType 'application/json' -Headers @{ 'x-functions-key' = 'OlEfvbvK6YHrBpQa8tIycbz49AR/mgasjZryR3f96TdyXb/ZsEWwaA==' }
+
+    Write-Output $response
+}
+
+for ($i = 0; $i -lt 25; $i++) {
+    $date = Get-date
+    $date = $date.AddDays(7 * $i + 2)
+
+    Write-Output "Storing event with date: $date"
+
+    PostToApi $date
 }
--- SportPlanner.ModelsDto/EventUserDto.cs
+++ SportPlanner.ModelsDto/EventUserDto.cs
@@ -6,7 +6,14 @@ namespace SportPlanner.ModelsDto
     {
         public Guid UserId { get; set; }
         public string UserName { get; set; } = string.Empty;
-        public int UserReply { get; set; }
+        public UserReply UserReply { get; set; }
         public bool IsOwner { get; set; }
+    }
+
+    public enum UserReply
+    {
+        Unknown = 0,
+        Going = 1,
+        NotGoing = 2
     }
 }
